7c79d85b9d7d442f89bc9411a476134663748828
dimstav23 <dimstav23@gmail.com>
2023-03-15 21:18:04 +0000

    add patch for new qemu build

--- scripts/common.sh
+++ scripts/common.sh
@@ -138,6 +138,7 @@ build_install_qemu()
 	MAKE="make -j $(getconf _NPROCESSORS_ONLN) LOCALVERSION="
 
 	pushd qemu >/dev/null
+		run_cmd git apply ../qemu.patch
 		run_cmd ./configure --target-list=x86_64-softmmu --prefix=$DEST --disable-werror
 		run_cmd $MAKE
 		run_cmd $MAKE install
